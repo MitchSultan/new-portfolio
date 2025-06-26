f3ef46751dcfc5a5f805c8fc619d821de67d5cd2
MitchSultan <admin@mitch.sultan.theotherguyske.org>
2025-06-26 16:58:55 -0600

    tryu

--- app/Blog/[id].js
+++ app/Blog/[id].js
@@ -1,35 +1,7 @@
-import { getAllPostSlugs, getPostHtml } from '@/lib/blog';
-import Head from 'next/head';
+import React from 'react'
 
-export default function Post({ post }: any) {
+export default function id() {
   return (
-    <div className="p-6">
-      <Head>
-        <title>{post.metadata.title}</title>
-      </Head>
-      <article>
-        <h1 className="text-3xl font-bold">{post.metadata.title}</h1>
-        <p className="text-sm text-gray-500">{post.metadata.date}</p>
-        <div
-          className="mt-4 prose max-w-none"
-          dangerouslySetInnerHTML={{ __html: post.contentHtml }}
-        />
-      </article>
-    </div>
-  );
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostSlugs();
-  return {
-    paths,
-    fallback: false, // or 'blocking' if you plan to add more posts later
-  };
-}
-
-export async function getStaticProps({ params }: any) {
-  const post = await getPostHtml(params.slug);
-  return {
-    props: { post },
-  };
+    <div>id</div>
+  )
 }
