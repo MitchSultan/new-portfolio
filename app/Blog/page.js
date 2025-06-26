ca5a293bec9f91e987fddab73731aa126d561dcd
MitchSultan <admin@mitch.sultan.theotherguyske.org>
2025-06-26 17:00:37 -0600

    ty

--- app/Blog/page.js
+++ app/Blog/page.js
@@ -1,29 +1,7 @@
-// pages/blog/index.js
-import Link from 'next/link';
-import { supabase } from '../../lib/supabaseClient';
+import React from 'react'
 
-export default function Blog({ posts }) {
+export default function page() {
   return (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.slug}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export async function getStaticProps() {
-  const { data: posts } = await supabase
-    .from('posts')
-    .select('id, title, slug')
-    .order('published_at', { ascending: false });
-
-  return { props: { posts } };
+    <div>page</div>
+  )
 }
