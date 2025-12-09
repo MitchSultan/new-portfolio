35cb473efb5216e5a82a8dddde8e0fbeecd40a46
MitchSultan <admin@mitch.sultan.theotherguyske.org>
2025-12-09 14:22:22 +0300

    gooe

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} ${outfit.variable}`} suppressHydrationWarning>
+    <html lang="en" className={`${inter.variable} ${outfit.variable}`}
+     suppressHydrationWarning>
+      <head>
+        <meta name="google-site-verification" content="8oWQjXaNYE-izREtHoHC5J0xHLTRLBnyFOQrE_mbfKw" />
+      </head>
       <body className="bg-white dark:bg-black text-gray-900 dark:text-gray-100 transition-colors duration-300 antialiased">
         <ClientProviders>
           {children}
