40130aa9d099fe743999f4864286f66c0a9d5cc9
MitchSultan <mitchsultan23@gmail.com>
2026-05-21 15:06:16 +0300

    footer

--- app/testimonial/page.js
+++ app/testimonial/page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import Testimonials from '../components/Testimonials';
 import Exper from '../components/exper';
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
 
 export default function page() {
   return (
